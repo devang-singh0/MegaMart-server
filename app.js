77ef05fa3d9eb64380eb46dea9fe33e23d40b8e7
devang <devangs528@gmail.com>
2024-04-11 09:48:44 +0530

    second commit

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ connectDB();
 // top level middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors({credentials: true}));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static('public'));
 app.use(isLoggedIn);
